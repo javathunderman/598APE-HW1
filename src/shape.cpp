48837f24b72dceecf9f6785d6ee426e3d8999ce3
Arjun <javathunderman@outlook.com>
2026-09-13 23:53:31 -0500

    Attempt more aggressive vectorization + replace custom sort w/ std::sort

--- src/shape.cpp
+++ src/shape.cpp
@@ -1,5 +1,10 @@
 #include "shape.h"
+#include <vector>
+#include <algorithm>
+#include <array>
 #include <iostream>
+#include <iterator>
+#include <string>
 
 Shape::Shape(const Vector &c, Texture* t, double ya, double pi, double ro): center(c), texture(t), yaw(ya), pitch(pi), roll(ro){
 };
@@ -37,30 +42,22 @@ typedef struct {
    Shape* shape;
 } TimeAndShape;
 
-void insertionSort(TimeAndShape *arr, int n) {
-    for (int i = 1; i < n; ++i) {
-        TimeAndShape key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j].time > key.time) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
-
 void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
    ShapeNode* t = c->listStart;
    size_t seen = 0;
    double time;
    unsigned int numShapes = c->numShapes;
-   TimeAndShape *times = (TimeAndShape*)malloc(sizeof(TimeAndShape)*numShapes);
+   std::vector<TimeAndShape> times;
+   times.reserve(numShapes);
    for (seen = 0; seen < numShapes; seen++) {
       time = t->data->getIntersection(ray);
-      times[seen] = (TimeAndShape){ time, t->data };
+      times.push_back((TimeAndShape){ time, t->data });
       t = t->next;
    }
-   insertionSort(times, seen);
+   std::sort(times.begin(), times.end(), [](const TimeAndShape &t1, const TimeAndShape &t2)
+   { 
+      return t1.time > t2.time;
+   });
 
    if (seen == 0 || times[0].time == inf) {
       double opacity, reflection, ambient;
@@ -75,7 +72,6 @@ void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
 
    double curTime = times[0].time;
    Shape* curShape = times[0].shape;
-   free(times);
 
    Vector intersect = curTime*ray.vector+ray.point;
    double opacity, reflection, ambient;
@@ -83,27 +79,26 @@ void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
    
    double lightData[3];
    getLight(lightData, c, intersect, curShape->getNormal(intersect), curShape->reversible());
-   toFill[0] = (unsigned char)(toFill[0]*(ambient+lightData[0]*(1-ambient)));
-   toFill[1] = (unsigned char)(toFill[1]*(ambient+lightData[1]*(1-ambient)));
-   toFill[2] = (unsigned char)(toFill[2]*(ambient+lightData[2]*(1-ambient)));
+   for (int j = 0; j < 3; j++) {
+      toFill[j] = (unsigned char)(toFill[j]*(ambient+lightData[j]*(1-ambient)));
+   }
    if(depth<c->depth && (opacity<1-1e-6 || reflection>1e-6)){
       unsigned char col[4];
       if(opacity<1-1e-6){
          Ray nextRay = Ray(intersect+ray.vector*1E-4, ray.vector);
          calcColor(col, c, nextRay, depth+1);
-         toFill[0]= (unsigned char)(toFill[0]*opacity+col[0]*(1-opacity));
-         toFill[1]= (unsigned char)(toFill[1]*opacity+col[1]*(1-opacity));
-         toFill[2]= (unsigned char)(toFill[2]*opacity+col[2]*(1-opacity));        
+         for (int j = 0; j < 3; j++) {
+            toFill[j]= (unsigned char)(toFill[j]*opacity+col[j]*(1-opacity));
+         }
       }
       if(reflection>1e-6){
          Vector norm = curShape->getNormal(intersect).normalize();
          Vector vec = ray.vector-2*norm*(norm.dot(ray.vector));
          Ray nextRay = Ray(intersect+vec*1E-4, vec);
          calcColor(col, c, nextRay, depth+1);
-      
-         toFill[0]= (unsigned char)(toFill[0]*(1-reflection)+col[0]*(reflection));
-         toFill[1]= (unsigned char)(toFill[1]*(1-reflection)+col[1]*(reflection));
-         toFill[2]= (unsigned char)(toFill[2]*(1-reflection)+col[2]*(reflection));
+         for (int j = 0; j < 3; j++) { // attempt to vectorize
+            toFill[j]= (unsigned char)(toFill[j]*(1-reflection)+col[j]*(reflection));   
+         }
       }
    }
 }
