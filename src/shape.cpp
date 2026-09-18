0b479370654dc1a6ca7f16ded528648cf5d9110c
Arjun Vedantham <arjunsv2@illinois.edu>
2026-09-18 17:22:32 -0500

    undo vectorization and just leave it unrolled

--- src/shape.cpp
+++ src/shape.cpp
@@ -79,26 +79,26 @@ void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
    
    double lightData[3];
    getLight(lightData, c, intersect, curShape->getNormal(intersect), curShape->reversible());
-   for (int j = 0; j < 3; j++) {
-      toFill[j] = (unsigned char)(toFill[j]*(ambient+lightData[j]*(1-ambient)));
-   }
+   toFill[0] = (unsigned char)(toFill[0]*(ambient+lightData[0]*(1-ambient)));
+   toFill[1] = (unsigned char)(toFill[1]*(ambient+lightData[1]*(1-ambient)));
+   toFill[2] = (unsigned char)(toFill[2]*(ambient+lightData[2]*(1-ambient)));
    if(depth<c->depth && (opacity<1-1e-6 || reflection>1e-6)){
       unsigned char col[4];
       if(opacity<1-1e-6){
          Ray nextRay = Ray(intersect+ray.vector*1E-4, ray.vector);
          calcColor(col, c, nextRay, depth+1);
-         for (int j = 0; j < 3; j++) {
-            toFill[j]= (unsigned char)(toFill[j]*opacity+col[j]*(1-opacity));
-         }
+         toFill[0]= (unsigned char)(toFill[0]*opacity+col[0]*(1-opacity));
+         toFill[1]= (unsigned char)(toFill[1]*opacity+col[1]*(1-opacity));
+         toFill[2]= (unsigned char)(toFill[2]*opacity+col[2]*(1-opacity));
       }
       if(reflection>1e-6){
          Vector norm = curShape->getNormal(intersect).normalize();
          Vector vec = ray.vector-2*norm*(norm.dot(ray.vector));
          Ray nextRay = Ray(intersect+vec*1E-4, vec);
          calcColor(col, c, nextRay, depth+1);
-         for (int j = 0; j < 3; j++) { // attempt to vectorize
-            toFill[j]= (unsigned char)(toFill[j]*(1-reflection)+col[j]*(reflection));   
-         }
+         toFill[0]= (unsigned char)(toFill[0]*(1-reflection)+col[0]*(reflection));
+         toFill[1]= (unsigned char)(toFill[1]*(1-reflection)+col[1]*(reflection));
+         toFill[2]= (unsigned char)(toFill[2]*(1-reflection)+col[2]*(reflection));
       }
    }
 }
